138f2bb114d677f99bebcf6b3ef0b61236e4d7ee
Joey <joey.s.hornsby@gmail.com>
2021-04-09 17:13:14 +0100

    expanded into fuller test suite

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,6 +1,6 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
-  const items = [
+  let items = [
     new Item("+5 Dexterity Vest", 10, 20),
     new Item("Aged Brie", 2, 0),
     new Item("Elixir of the Mongoose", 5, 7),
@@ -8,39 +8,89 @@ describe("Gilded Rose", function() {
     new Item("Sulfuras, Hand of Ragnaros", -1, 80),
     new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
     new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 47),
     new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 46),
   ]
-  const gildedRose = new Shop(items)
+// some tests here - that sellIn ALWAYS goes down by 1, that quality NEVER goes above 50 and that quality is NEVER negative. start with the most straightforward cases.
+  describe("updateQuality", function(){
+    describe("for normal items", function(){
+      let normalItems = [
+        new Item("Chonky", 10, 20),
+        new Item("Beans", 5, 0),
+        new Item("Peanuts", 0, 4),
+      ]
+      it("ages items appropriately before sell-by date passed", function(){
+        let gildedRose = new Shop([normalItems[0], normalItems[1]])
+        let updatedItems = gildedRose.updateQuality()
+        expect(updatedItems[0].name).toEqual("Chonky")
+        expect(updatedItems[0].sellIn).toEqual(9)
+        expect(updatedItems[0].quality).toEqual(19)
+        expect(updatedItems[1].name).toEqual("Beans")
+        expect(updatedItems[1].sellIn).toEqual(4)
+        expect(updatedItems[1].quality).toEqual(0)
 
-  it("ages items appropriately", function() {
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("+5 Dexterity Vest");
-    expect(items[0].sellIn).toEqual(9);
-    expect(items[0].quality).toEqual(19);
-    expect(items[1].name).toEqual("Aged Brie");
-    expect(items[1].sellIn).toEqual(1);
-    expect(items[1].quality).toEqual(1);
-    expect(items[2].name).toEqual("Elixir of the Mongoose");
-    expect(items[2].sellIn).toEqual(4);
-    expect(items[2].quality).toEqual(6);
-    expect(items[3].name).toEqual("Sulfuras, Hand of Ragnaros");
-    expect(items[3].sellIn).toEqual(0);
-    expect(items[3].quality).toEqual(80);
-    expect(items[4].name).toEqual("Sulfuras, Hand of Ragnaros");
-    expect(items[4].sellIn).toEqual(-1);
-    expect(items[4].quality).toEqual(80);
-    expect(items[5].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[5].sellIn).toEqual(14);
-    expect(items[5].quality).toEqual(21);
-    expect(items[6].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[6].sellIn).toEqual(9);
-    expect(items[6].quality).toEqual(50);
-    expect(items[7].name).toEqual("Backstage passes to a TAFKAL80ETC concert");
-    expect(items[7].sellIn).toEqual(4);
-    expect(items[7].quality).toEqual(50);
-    
-  });
+      })
+      it("ages items appropriately after sell-by date passed", function(){
+        let gildedRose = new Shop([normalItems[2]])
+        let updatedItems = gildedRose.updateQuality()
+        expect(updatedItems[0].name).toEqual("Peanuts")
+        expect(updatedItems[0].sellIn).toEqual(-1)
+        expect(updatedItems[0].quality).toEqual(2)
 
+      })
+    })
+    describe("for brie", function(){
+      let bries = [
+        new Item("Aged Brie", 5, 4),
+        new Item("Aged Brie", -1, 3)
+      ]
+      it("increases quality of the brie", function(){
+        let gildedRose = new Shop(bries)
+        let newBrie = gildedRose.updateQuality()
+        expect(newBrie[0].sellIn).toEqual(4)
+        expect(newBrie[0].quality).toEqual(5)
+        expect(newBrie[1].sellIn).toEqual(-2)
+        expect(newBrie[1].quality).toEqual(5)
+      })
+    })
+  })
+  describe("for Sulfuras, Hand of Ragnaros", function(){
+    let sulfurases = [
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    ]
+    it("increases quality of the brie", function(){
+      let gildedRose = new Shop(sulfurases)
+      let newBrie = gildedRose.updateQuality()
+      expect(sulfurases[0].sellIn).toEqual(0)
+      expect(sulfurases[0].quality).toEqual(80)
+      expect(sulfurases[1].sellIn).toEqual(-1)
+      expect(sulfurases[1].quality).toEqual(80)
+    })
+  })
+  describe("for backstage passes", function(){
+    let backstagePasses = [
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 46),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 43),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 46),
+    ]
+    it("increases quality of the brie", function(){
+      let gildedRose = new Shop(backstagePasses)
+      let newBP = gildedRose.updateQuality()
+      expect(newBP[0].sellIn).toEqual(14)
+      expect(newBP[0].quality).toEqual(21)
+      expect(newBP[1].sellIn).toEqual(9)
+      expect(newBP[1].quality).toEqual(48)
+      expect(newBP[2].sellIn).toEqual(4)
+      expect(newBP[2].quality).toEqual(46)
+      expect(newBP[3].sellIn).toEqual(9)
+      expect(newBP[3].quality).toEqual(50)
+      expect(newBP[4].sellIn).toEqual(-1)
+      expect(newBP[4].quality).toEqual(0)
+    })
+  })
+})
 
-
-});
