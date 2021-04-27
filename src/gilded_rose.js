1d69026e85d8cae7708cc47f4546cfce8434d923
Joey <joey.s.hornsby@gmail.com>
2021-04-27 10:09:18 +0100

    added decrese quality method to check for min quality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,6 +15,11 @@ class Shop {
       item.quality ++
     }
   }
+  decreaseQuality(item) {
+    if (item.quality > 0) {
+      item.quality --
+    }
+  }
   agedBrie(agedbrie){
       this.increaseQuality(agedbrie)
       if (agedbrie.sellIn < 0) {
@@ -46,37 +51,11 @@ class Shop {
         this.backstagePasses(item)
         continue
       }
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-            item.quality --;
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality ++;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality ++;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality ++;
-              }
-            }
-          }
-        }
-      }
+      this.decreaseQuality(item)
       if (item.sellIn < 0) {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-                item.quality --;
-            }
-          } else {
-            item.quality = 0;
-          }
-        } 
-        }
+        this.decreaseQuality(item)
+         }
+      }
 
     return this.items;
   }
