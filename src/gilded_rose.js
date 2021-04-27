320e0c2c847ba786aeb0d5acd6349631cca6d25b
Joey <joey.s.hornsby@gmail.com>
2021-04-27 09:38:20 +0100

    refactored aged brie into own function

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -10,11 +10,23 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+  agedBrie(agedbrie){
+      if (agedbrie.quality < 49) {
+        agedbrie.quality ++
+      }
+      if (agedbrie.sellIn < 0 && agedbrie.quality < 49) {
+        agedbrie.quality ++
+      }
+  }
   updateQuality() {
     for (var item of this.items) {
       if (item.name === 'Sulfuras, Hand of Ragnaros') return item
       item.sellIn = item.sellIn - 1;
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name === 'Aged Brie') {
+        this.agedBrie(item)
+        continue
+      }
+      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > 0) {
             item.quality --;
         }
@@ -36,7 +48,6 @@ class Shop {
         }
       }
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (item.quality > 0) {
                 item.quality --;
@@ -44,13 +55,8 @@ class Shop {
           } else {
             item.quality = 0;
           }
-        } else {
-          if (item.quality < 50) {
-            item.quality ++;
-          }
+        } 
         }
-      }
-    }
 
     return this.items;
   }
